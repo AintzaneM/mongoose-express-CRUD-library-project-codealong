8b36924535bf78616b795746b2ff6534fd9489e5
Aintzane Morales <aintzanemorales1992@gmail.com>
2021-10-18 18:00:42 +0200

    Iteration to create a book on DB

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -13,11 +13,51 @@ router.get("/books", (req, res, next) => {
 
     }) 
 
-    .catch(() => {
+    .catch((err) => {
+        console.log("ups, an error has been detected with displaying a list of books", err)
+        next(error);
 
     })
 
     
 });
 
+router.get("/books/create", (req, res, next) =>{
+    res.render("books/book-create")
+})
+
+router.post("/books/create", (req, res, next) =>{
+    //console.log(req.body)
+    const {title, author, description, rating} = req.body;
+    Book.create({title, author, description, rating})
+    .then(() => {
+        
+        res.redirect("/books")
+
+    })
+
+    .catch((err) => {
+        console.log("ups, an error has been detected with displaying a new book", err)
+
+    })
+})
+
+router.get("/books/:bookId", (req, res, next )=>{
+    Book.findById(req.params.bookId)
+    .then((booksFromDB) => {
+        console.log(booksFromDB)
+        res.render("books/books-details", booksFromDB)
+    })
+
+    .catch ((error)=> {
+        console.log("ups, an error occured with the book ID", error)
+        next (error);
+    })
+
+})
+
+
+
+
 module.exports = router
+
